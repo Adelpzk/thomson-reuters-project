a96428f9ff292cc5fa89f6b0a3b732da5977c18e
Adel Pazoki <apazokit@uwaterloo.ca>
2021-07-14 12:17:34 -0400

    first appearance

--- thomson-reuters-graphql/index.js
+++ thomson-reuters-graphql/index.js
@@ -4,16 +4,16 @@ require("dotenv").config({ path: "../.env" });
 
 var url = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@salesforce-data.xg1r1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-/*
+
 MongoClient.connect(url, function (err, db) {
   if (err) throw err;
   var dbo = db.db("TomsonReutersData");
   var coll = dbo.collection("salesforceData");
 
-  coll.find({ id: 1 }).toArray(function (err, result) {
+  coll.find({ id: 2 }).toArray(function (err, result) {
     if (err) throw err;
     console.log(result);
     db.close();
   });
 });
-*/
+
