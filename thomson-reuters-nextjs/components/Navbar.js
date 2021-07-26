15c1a2fff257b9b82563caf06b7465f03db9ae01
Jugal Bilimoria <jugalbili@yahoo.ca>
2021-07-26 14:37:05 -0400

    Removed Home NavItem

--- thomson-reuters-nextjs/components/Navbar.js
+++ thomson-reuters-nextjs/components/Navbar.js
@@ -5,21 +5,25 @@ export default function Navbar() {
   return (
     <div className="tr-SiteHeader tr-SiteHeader--withSiteName">
       <nav className="nav-bar tr-SiteHeader-inner">
-        <div className="nav-logo tr-SiteHeader-logo">
-          <Image
-            src="/tr_logo.png"
-            className="app-logo"
-            height={55}
-            width={235}
-          />
-        </div>
+        <a style={{ cursor: "pointer" }}>
+          <Link href="/">
+            <div className="nav-logo tr-SiteHeader-logo">
+              <Image
+                src="/tr_logo.png"
+                className="app-logo"
+                height={55}
+                width={235}
+              />
+            </div>
+          </Link>
+        </a>
         <div className="nav-pages">
           <ul className="tr-SiteHeader-menus">
-            <li className="tr-SiteHeaderMenu">
+            {/* <li className="tr-SiteHeaderMenu">
               <Link href="/">
                 <a className="tr-SiteHeaderMenu-link">Home</a>
               </Link>
-            </li>
+            </li> */}
             <li className="tr-SiteHeaderMenu">
               <Link href="/blogs">
                 <a className="tr-SiteHeaderMenu-link">Blogs</a>
